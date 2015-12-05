38e1d458ead6e9e5f09e8afee517fc113761cb87
John Martin <john.itvn@gmail.com>
2015-12-05 09:41:45 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,7 +1,9 @@
 sudo yum install wget -y
-wget https://github.com/johnitvn/busycent/archive/master.tar.gz -O /tmp/busycent.tar.gz
-tar -xzvf /tmp/busycent.tar.gz
-cp /tmp/busycent-master /usr/bin/
-cp busycent.sh busycent
-chmod +X /usr/bin/*
+sudo wget https://github.com/johnitvn/busycent/archive/master.tar.gz -O /tmp/busycent.tar.gz
+sudo tar -xzvf /tmp/busycent.tar.gz
+sudo rm -rf /usr/share/busycent
+sudo mkdir /usr/share/busycent
+sudo cp /tmp/busycent-master/* /usr/share/busycent
+sudo chmod +X /usr/share/busycent/*
+ln -s /usr/share/busycent/busycent.sh /usr/bin/busycent
 
