eda2f137f50f02fb9ae5618c150c36a277ee1274
John Martin <john.itvn@gmail.com>
2015-12-07 01:54:17 +0700

    Update install_mariadb.sh

--- scripts/install_mariadb.sh
+++ scripts/install_mariadb.sh
@@ -3,6 +3,6 @@ sudo wget -O /etc/yum.repos.d/MariaDB.repo http://mariadb.if-not-true-then-false
 sudo yum clean
 sudo yum update -y
 sudo yum install MariaDB-server MariaDB-client -y
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
+sudo systemctl start mysql
+sudo systemctl enable mysql
 sudo mysql_secure_installation
