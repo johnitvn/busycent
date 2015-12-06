2bddf54396c960ee5d3ab6361b6bdb8f5977580c
John Martin <john.itvn@gmail.com>
2015-12-07 01:06:00 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,15 +2,15 @@ echo "Install depends"
 sudo yum install wget nano -y
 echo ""
 echo "Download busycent package"
-sudo wget https://github.com/johnitvn/busycent/archive/master.tar.gz -O /tmp/busycent.tar.gz
+sudo wget https://github.com/johnitvn/busycent/archive/master.tar.gz -O ~/busycent.tar.gz
 echo ""
 echo "Uncompress busycent package"
-sudo tar -xzvf /tmp/busycent.tar.gz
+sudo tar -xzvf  ~/busycent.tar.gz
 echo ""
 echo "Clean old version and install"
 sudo rm -rf /usr/share/busycent
 sudo mkdir /usr/share/busycent
-sudo cp -r /tmp/busycent-master/* /usr/share/busycent
+sudo cp -r ~/busycent-master/* /usr/share/busycent
 echo ""
 echo "Make runable"
 sudo chmod +X /usr/share/busycent/*
